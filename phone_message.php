125d6d08340e289dda53b424d13e94a7edc918ed
eMiracle <miraclechibuzo@gmail.com>
2020-06-09 20:09:56 +0100

    update

--- phone_message.php
+++ phone_message.php
@@ -1,64 +1,96 @@
 <?php
-header("Content-Type:application/json");
+require_once "db.php";///////database connection
+require_once "functions.php";///////all functions
+require_once 'Twilio/autoload.php';/////////load Twilio
 
-////////////credentials
-$mysid = "";///////my sid
-$mytoken = "";///////my token
-$Twiliophone = ""; ///////paste twilio phone
-/////////////////
-require_once 'Twilio/autoload.php';
 use Twilio\Rest\Client;
 
-$phone = $_GET['phone'];
-$messagen = $_GET['message'];
+  $accesvia=$_SERVER["REQUEST_METHOD"];///////how its being accessed
 
-if (isset($phone) && $phone !="" && isset($messagen) && $messagen !="") {
- 
-$sid    = $mysid;
-$token  = $mytoken;
+  switch($request_method)
+  {
+    case 'POST':
 
-function filter($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
+      /////Post email
+      if(!empty($_POST))/////////make sure post query holds value
+      {
+       ///////////////////////filter data
+        $id = filter($_POST['id']);
+        $key = filter($_POST['key']);
+        $phone = $_POST['phone'];
+        $messagen = $_POST['message'];
+       //////////////////////////////
 
-function validate_phone($number)
-{
-     // Allow +, - and . in phone number
-     $filtered_phone = filter_var($number, FILTER_SANITIZE_NUMBER_INT);
-     // Remove "-" from number
+        ////////////Check Unit Balance
+        ///get balance from data base////////////////
+          $stmt = $con->prepare("SELECT * FROM user WHERE sid = ? and api_key = ?");
+          $stmt->bind_param("s", $id);
+          $stmt->bind_param("s", $key);
+          $stmt->execute();
+          $result = $stmt->get_result()()->fetch_all(MYSQLI_ASSOC);
+          if($result->num_rows < 1) {
+            $response=array(
+                'status' => 419,
+                'status_message' =>'You dont have an account with us.'
+              );
+          }
+          $stmt->close();
+          $email = $result[0]['email'];
+          $balance = $result[0]['unit'];
+          //////////////////////
 
-     $phone_to_check = str_replace("-", "", $filtered_phone);
-     // Check the lenght of number
+          if ($balance<1) {
+            $response=array(
+                'status' => 420,
+                'status_message' =>'You dont have any units in your account.'
+              );
+          }else{
 
-     // This can be customized if you want phone number from a specific country
-     if (strlen($phone_to_check) < 11 || strlen($phone_to_check) > 14) {
-        return false;
-     } else {
-       return true;
-     }
-}
+      $newmessage = filter($messagen);////////get message from the string
+      $to = validate_phone($phone);/////phone forward message to
 
-$newmessage = filter($messagen);////////get message from the string
-$to = validate_phone($phone);/////phone forward message to
+      $twilio = new Client($sid, $auth);
 
-$twilio = new Client($sid, $token);
-
-$message = $twilio->messages
-                  ->create($to, // to
+      $message = $twilio->messages
+                  ->create($phone, // to
                            [
                                "body" => $newmessage,
-                               "from" => $Twiliophone
+                               "from" => $TwilioPhone
                            ]
                   );
+      }
+                  ////////////reponse coding
+      if(!empty($message->sid)){
 
-//print($message->sid);
+        /////////update unit balance
+          $stmt = $con->prepare("UPDATE user SET unit = ? WHERE sid = ? and api_key = ?");
+          $stmt->bind_param("ss", $id, $key);
+          $stmt->execute();
+          $stmt->close();
+          //////////////////
+          
+        $response=array(
+                'status' => 1,
+                'status_message' =>'Message Sent Successfully.'
+              );
+            }
+            else
+            {
+              $response=array(
+                'status' => 0,
+                'status_message' =>'Message sending Failed.'
+              );
+            }
+  header('Content-Type: application/json');
+  echo json_encode($response);
 
-}elseif ($_GET['phone']=="" OR $_GET['message']=="") {
-  echo "phone or mesage can't be empty";
-}else{
-  echo "an error occured contact admin";
 }
+      
+      break;
+    
+    default:
+      // Invalid Request Method
+      header("HTTP/1.0 405 Method Not Allowed");
+      break;
+  }
 ?>
